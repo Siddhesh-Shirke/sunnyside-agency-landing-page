4bfe4851a8709fe5b61cd54f4a4a620a9aaeea06
Siddhesh <shirke0personal@gmail.com>
2022-04-19 18:21:53 +0530

    Pre footer image gallery added.

--- src/components/PreFooterDecor.js
+++ src/components/PreFooterDecor.js
@@ -3,10 +3,13 @@ import React from "react";
 export default function PreFooterDecor(){
     return(
         <section className="pre--footer--decor">
-            <img src="https://siddheshshirke.files.wordpress.com/2022/04/image-gallery-milkbottles.jpg" alt=""/>
-            <img src="https://siddheshshirke.files.wordpress.com/2022/04/image-gallery-orange.jpg" alt=""/>
-            <img src="https://siddheshshirke.files.wordpress.com/2022/04/image-gallery-cone.jpg" alt=""/>
-            <img src="https://siddheshshirke.files.wordpress.com/2022/04/image-gallery-sugarcubes.jpg" alt=""/>
+            <div className="img--gallery--milkbottles"></div>
+            <div className="img--gallery--orange"></div>
+            <div className="img--gallery--cone"></div>
+            <div className="img--gallery--sugarcubes"></div>
         </section>
     )
 }
+
+
+ 
